abbc5f69f3cca3d5c1ae31955c693eae05d7065e
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-29 15:34:08 +0200

    Initial commit

---
+++ DisableAfterTime.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DisableAfterTimeEx {
+
+	public class DisableAfterTime : MonoBehaviour{
+
+		/// Game object to disable.
+		[SerializeField]
+		private GameObject _target;
+
+		/// Delay before disabling the target game object.
+		[SerializeField]
+		private float _delay = 0;
+
+		private void Start () {
+			if (!_target) {
+				Utilities.MissingReference(this, "target");
+			}
+
+            Invoke("DisableTarget", _delay);
+		}
+
+	    private void DisableTargetGO() {
+	        _target.SetActive(false);
+	    }
+	}
+}
---
+++ Editor/DisableAfterTimeEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using OneDayGame;
+
+namespace DisableAfterTimeEx {
+
+    [CustomEditor(typeof (DisableAfterTime))]
+    public class DisableAfterTimeEditor : Editor {
+
+        private SerializedProperty _target;
+        private SerializedProperty _delay;
+
+        private void OnEnable() {
+            _target = serializedObject.FindProperty("_target");
+            _delay = serializedObject.FindProperty("_delay");
+        }
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(_target);
+            EditorGUILayout.PropertyField(_delay);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+
+}
