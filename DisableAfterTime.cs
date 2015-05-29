4a39e008b876f13c3102ffeb437784ce675d5a94
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-29 15:36:17 +0200

    Untabify

--- DisableAfterTime.cs
+++ DisableAfterTime.cs
@@ -3,26 +3,26 @@ using System.Collections;
 
 namespace DisableAfterTimeEx {
 
-	public class DisableAfterTime : MonoBehaviour{
+    public class DisableAfterTime : MonoBehaviour{
 
-		/// Game object to disable.
-		[SerializeField]
-		private GameObject _target;
+        /// Game object to disable.
+        [SerializeField]
+        private GameObject _target;
 
-		/// Delay before disabling the target game object.
-		[SerializeField]
-		private float _delay = 0;
+        /// Delay before disabling the target game object.
+        [SerializeField]
+        private float _delay = 0;
 
-		private void Start () {
-			if (!_target) {
-				Utilities.MissingReference(this, "target");
-			}
+        private void Start () {
+            if (!_target) {
+                Utilities.MissingReference(this, "target");
+            }
 
             Invoke("DisableTarget", _delay);
-		}
+        }
 
-	    private void DisableTargetGO() {
-	        _target.SetActive(false);
-	    }
-	}
+        private void DisableTargetGO() {
+            _target.SetActive(false);
+        }
+    }
 }
